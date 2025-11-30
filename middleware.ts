66e86a0a61e8ec7e9a0d3bff0b98cea016781706
Steve Persch <steve.persch@pantheon.io>
2025-11-30 16:53:24 -0600

    redirecting live env to real domain name

--- middleware.ts
+++ middleware.ts
@@ -18,7 +18,8 @@ export function middleware(request: NextRequest) {
   // x-proto
   // Is the site Machine name available as an env var?
   const siteMachineName = "office-artifacts";
-  const incomingProtocol = request.headers.get('x-proto');
+  const siteEnv = request.headers.get('pcontext-site-env') || '';
+  const incomingProtocol = request.headers.get('x-proto') || '';
   const policyDocSurrogateKey = request.headers.get('policy-doc-surrogate-key') || '';
   console.log('incomingProtocol: ' + incomingProtocol + ' policyDocSurrogateKey: ' + policyDocSurrogateKey);
   if (incomingProtocol === 'http://' && policyDocSurrogateKey && policyDocSurrogateKey.trim().endsWith(siteMachineName + '.pantheonsite.io')) {
@@ -28,6 +29,17 @@ export function middleware(request: NextRequest) {
     url.port = "";
     // Use a 301 permanent redirect
     return NextResponse.redirect(url.toString(), 301);
+  }
+
+  if (siteEnv === 'pr-17' || siteEnv === 'live') {
+    console.log('inside siteEnv check');
+    if (policyDocSurrogateKey === 'pr-17-office-artifacts.pantheonsite.io' || policyDocSurrogateKey === 'live-office-artifacts.pantheonsite.io') {
+      url.protocol = "https:";
+      url.hostname = "office-artifacts.stevector.com";
+      url.port = "";
+      // Use a 301 permanent redirect
+      return NextResponse.redirect(url.toString(), 301);
+    }
   }
 
   const response = NextResponse.next();
