4ef6eba6495831952cd60e643f37dfaf66180ffe
Steve Persch <steve.persch@pantheon.io>
2026-02-09 18:29:45 -0600

    removing old code

--- middleware.ts
+++ middleware.ts
@@ -26,42 +26,6 @@ export function middleware(request: NextRequest) {
       return NextResponse.redirect(url.toString(), 301);
   }
 
-
-
-  
-
-  //x-fastly-orig-host
-  // pcontext-site-env
-  // x-proto
-  // Is the site Machine name available as an env var?
-  const siteMachineName = "office-artifacts";
-  const siteEnv = request.headers.get('pcontext-site-env') || '';
-  const incomingProtocol = request.headers.get('x-proto') || '';
-  const policyDocSurrogateKey = request.headers.get('policy-doc-surrogate-key') || '';
-  console.log('incomingProtocol: ' + incomingProtocol + ' policyDocSurrogateKey: ' + policyDocSurrogateKey);
-  if (incomingProtocol === 'http://' && policyDocSurrogateKey) {
-    if(policyDocSurrogateKey.trim().endsWith(siteMachineName + '.pantheonsite.io') ||
-      policyDocSurrogateKey === 'office-artifacts.stevector.com') {
-
-    url.protocol = "https:";
-    url.hostname = policyDocSurrogateKey;
-    url.port = "";
-    // Use a 301 permanent redirect
-    return NextResponse.redirect(url.toString(), 301);
-    }
-  }
-
-  if (siteEnv === 'pr-17' || siteEnv === 'live') {
-    console.log('inside siteEnv check');
-    if (policyDocSurrogateKey === 'pr-17-office-artifacts.pantheonsite.io' || policyDocSurrogateKey === 'live-office-artifacts.pantheonsite.io' || policyDocSurrogateKey === 'office-artifacts-redirect.stevector.com') {
-      url.protocol = "https:";
-      url.hostname = "office-artifacts.stevector.com";
-      url.port = "";
-      // Use a 301 permanent redirect
-      return NextResponse.redirect(url.toString(), 301);
-    }
-  }
-
   const response = NextResponse.next();
 
   return response;
